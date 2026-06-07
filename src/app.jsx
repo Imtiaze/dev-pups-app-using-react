ef659efa284cc8ce7e7b769dfc0d68518cade664
Imtiaze <ahammedimtiaze78@gmail.com>
2026-06-07 19:40:34 +0600

    Refactore the setup

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,46 @@
-const tech = "React, Tailwind and Vite";
-
 export function App() {
-  return <h1 className="font-mono text-4xl">Hello {tech}!</h1>;
+    return (
+      <PageWrapper>
+        <Container>
+          <Header />
+        </Container>
+      </PageWrapper>
+    )
+}  
+
+function PageWrapper({children}) {
+  return (
+     <main className="min-h-dvh bg-gradient-to-b from-cyan-200 to-white to-[60vh]">{children}</main>
+  )
+}
+
+function Container({children}) {
+  return (
+    <div className="mx-auto max-w-5xl p-4 md:p-8">{children}</div>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+          {/* Logo */}
+          <a className="group" href="/">
+            <div className="inline-flex items-center gap-4">
+              <img
+                src="/images/logo.png"
+                alt="DevPups"
+                className="h-16 transition group-hover:scale-105 group-hover:-rotate-6 md:h-20 lg:h-24"
+              />
+              <p className="text-lg font-semibold">Dev Pups</p>
+            </div>
+          </a>
+          {/* Hero copy */}
+          <div className="mt-6">
+            <h1 className="text-lg font-bold">We've got the best puppies!</h1>
+            <p className="text-slate-600">
+              Don't take our word — let the pictures do the talking :)
+            </p>
+          </div>
+        </header>
+  )
 }

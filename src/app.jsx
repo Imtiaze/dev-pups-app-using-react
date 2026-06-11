ed9a4e3f945b820b1c1965994b31e2d4d3182479
Imtiaze <ahammedimtiaze78@gmail.com>
2026-06-11 16:01:48 +0600

    All Templating done

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,27 @@
 import { PageWrapper } from "./components/PageWrapper"
 import { Container } from "./components/Container"
-
 import { Header } from "./components/Header"
+import { Search } from "./components/Search"
+import { ShortList } from "./components/ShortList"
+import { NewPuppyForm } from "./components/NewPuppyForm"
 
 export function App() {
     return (
       <PageWrapper>
         <Container>
           <Header />
-          <main>
-            
-          </main>
+            <Main />
         </Container>
       </PageWrapper>
     )
 }  
+
+function Main() {
+  return (
+    <main>
+      <Search />
+      <ShortList />
+      <NewPuppyForm />
+    </main>
+  )
+}
